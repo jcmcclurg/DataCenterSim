977c1a3ffaab956cba28067203c347e014f5f9b2
Josiah McClurg <jcmcclurg@josiah-mcclurgs-imac.local>
2013-12-10 18:01:52 -0600

    Initial Commit

---
+++ DataCenterSim/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DataCenterSim
+//
+//  Created by Josiah McClurg on 12/10/13.
+//  Copyright (c) 2013 Josiah McClurg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
